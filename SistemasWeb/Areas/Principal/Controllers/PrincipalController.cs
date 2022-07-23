d6f85dabca3cd40c841e250177030d4a4bd191d4
JESUS EDUARDO <j_edi_24@hotmail.com>
2022-07-23 13:44:41 -0500

    todo correcto hasta el video 30

---
+++ SistemasWeb/Areas/Principal/Controllers/PrincipalController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemasWeb.Areas.Principal.Controllers
+{
+    [Area("Principal")]
+    public class PrincipalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
